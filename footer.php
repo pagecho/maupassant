0225198f85108fa213d0d4dfaed7c0572b818e96
Chao <hello@pagecho.com>
2014-01-12 18:44:43 +0800

    modify a .

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 <footer id="footer">
 	<div class="container">
 		&copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
-		<?php _e('由 <a href="http://www.typecho.org">Typecho</a> 强力驱动. 模板由<a href="http://pagecho.com">cho</a>制作.'); ?>.
+		<?php _e('由 <a href="http://www.typecho.org">Typecho</a> 强力驱动. 模板由<a href="http://pagecho.com">cho</a>制作'); ?>.
 	</div>
 </footer>
 <?php $this->footer(); ?>
