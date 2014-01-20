086b6dc89f4cc59f55d518a34e8b80c4888dae2c
Chao <hello@pagecho.com>
2014-01-20 12:50:06 +0800

    add archive-page and the modification of style.css

---
+++ page-archive.php
@@ -0,0 +1,34 @@
+<?php    
+   /**  
+    * archives    
+    * @package custom   
+    */    
+$this->need('header.php');?>   
+<div class="col-8" id="main">
+	<div class="res-cons">
+            <article class="post">
+                <div class="post-content-pages">
+        			<?php $this->widget('Widget_Contents_Post_Recent', 'pageSize=10000')->to($archives);   
+            $year=0; $mon=0; $i=0; $j=0;   
+            $output = '<div id="archives">';   
+            while($archives->next()):   
+            $year_tmp = date('Y',$archives->created);   
+             $mon_tmp = date('m',$archives->created);   
+             //var_dump($year_tmp);   
+             $y=$year; $m=$mon;   
+             if ($mon != $mon_tmp && $mon > 0) $output .= '</ul></li>';   
+             if ($year != $year_tmp && $year > 0) $output .= '</ul>';   
+             if ($year != $year_tmp) {   
+                 $year = $year_tmp;   
+                 $output .= '<div class="al_year">'. $year .' 年</div><ul class="al_mon_list">'; //输出年份   
+             }   
+             $output .= '<li>'.date('m/d',$archives->created).'<a href="'.$archives->permalink .'">'. $archives->title .'</a> <em>('. $archives->commentsNum.')</em></li>'; //输出文章日期和标题   
+        endwhile;   
+        $output .= '</ul></li></ul></div>';   
+        echo $output;   
+        ?>
+                </div>
+    		</article>
+	</div>
+</div>
+<?php $this->need('footer.php'); ?>
