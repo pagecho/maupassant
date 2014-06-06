b186c366f51130fe9c146280c070c32d82c3a0dd
pickcho <tangbaikai@googlemail.com>
2014-06-07 02:07:17 +0800

    Version 1.3

--- footer.php
+++ footer.php
@@ -3,15 +3,9 @@
 </div>
 <footer id="footer">
 	<div class="container">
-		&copy; <?php echo date('Y'); ?> <a rel="nofollow" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
+		&copy; <?php echo date('Y'); ?> <a rel="nofollow" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>. 模板由<a href="http://pickcho.com">cho</a>制作.
 	</div>
 </footer>
 <?php $this->footer(); ?>
-<div class="site-search">
-<script language="javascript" type="text/javascript" src="http://js.users.51.la/17068114.js"></script>
-<script type="text/javascript">
-　　if (window!=top){top.location.href = window.location.href;}
-</script>
-</div>
 </body>
 </html>
