fc1681f0fcbdb204313fa4c123f944a92372695e
cho <pagecho@users.noreply.github.com>
2014-10-16 23:05:02 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <!DOCTYPE HTML>
+<html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
