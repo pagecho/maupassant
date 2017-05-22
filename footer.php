a95a66c30e3f25a7121138fbe5b3fa91771c9a71
8023 <lxy_05@126.com>
2017-05-22 21:20:56 +0800

    tab to space, easy table style, breadcrumbs, reply style

--- footer.php
+++ footer.php
@@ -1,10 +1,20 @@
-		</div>
+        </div>
     </div>
 </div>
 <footer id="footer">
-	<div class="container">
-		&copy; <?php echo date('Y'); ?> <a rel="nofollow" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>. 模板由<a href="http://pagecho.com">cho</a>制作.
-	</div>
+    <div class="container">
+        <div class="crumbs_patch">
+            <a href="<?php $this->options->siteUrl(); ?>">首页</a> &raquo;</li>
+            <?php if ($this->is('index')): ?><!-- 页面为首页时 -->
+                最新博文
+            <?php elseif ($this->is('post')): ?><!-- 页面为文章单页时 -->
+                <?php $this->category(); ?> &raquo; <?php $this->title() ?>
+            <?php else: ?><!-- 页面为其他页时 -->
+                <?php $this->archiveTitle(' &raquo; ','',''); ?>
+            <?php endif; ?>
+        </div>
+        &copy; <?php echo date('Y'); ?> <a rel="nofollow" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>. 模板由<a href="http://pagecho.com">cho</a>制作.
+    </div>
 </footer>
 <?php $this->footer(); ?>
 </body>
--- sidebar.php
+++ sidebar.php
@@ -1,14 +1,14 @@
 <div id="secondary">
-	<section class="widget">
+    <section class="widget">
         <form id="search" method="post" action="./">
             <input type="text" name="s" class="text" placeholder="搜索..." />
             <button type="submit" class="submit icon-search">搜索</button>
         </form>
     </section>
 
-	<?php if (!empty($this->options->sidebarBlock) && in_array('ShowRecentPosts', $this->options->sidebarBlock)): ?>
+    <?php if (!empty($this->options->sidebarBlock) && in_array('ShowRecentPosts', $this->options->sidebarBlock)): ?>
     <section class="widget">
-		<h3 class="widget-title"><?php _e('最新文章'); ?></h3>
+        <h3 class="widget-title"><?php _e('最新文章'); ?></h3>
         <ul class="widget-list">
             <?php $this->widget('Widget_Contents_Post_Recent')
             ->parse('<li><a href="{permalink}">{title}</a></li>'); ?>
@@ -18,7 +18,7 @@
 
     <?php if (!empty($this->options->sidebarBlock) && in_array('ShowRecentComments', $this->options->sidebarBlock)): ?>
     <section class="widget">
-		<h3 class="widget-title"><?php _e('最近回复'); ?></h3>
+        <h3 class="widget-title"><?php _e('最近回复'); ?></h3>
         <ul class="widget-list">
         <?php $this->widget('Widget_Comments_Recent')->to($comments); ?>
         <?php while($comments->next()): ?>
@@ -30,30 +30,30 @@
 
     <?php if (!empty($this->options->sidebarBlock) && in_array('ShowCategory', $this->options->sidebarBlock)): ?>
     <section class="widget">
-		<h3 class="widget-title"><?php _e('分类'); ?></h3>
+        <h3 class="widget-title"><?php _e('分类'); ?></h3>
         <ul class="widget-list">
             <?php $this->widget('Widget_Metas_Category_List')
             ->parse('<li><a href="{permalink}">{name}</a> ({count})</li>'); ?>
         </ul>
-	</section>
+    </section>
     <?php endif; ?>
 
     <?php if (!empty($this->options->sidebarBlock) && in_array('ShowArchive', $this->options->sidebarBlock)): ?>
     <section class="widget">
-		<h3 class="widget-title"><?php _e('归档'); ?></h3>
+        <h3 class="widget-title"><?php _e('归档'); ?></h3>
         <ul class="widget-list">
             <?php $this->widget('Widget_Contents_Post_Date', 'type=month&format=F Y')
             ->parse('<li><a href="{permalink}">{date}</a></li>'); ?>
         </ul>
-	</section>
+    </section>
     <?php endif; ?>
 
     <?php if (!empty($this->options->sidebarBlock) && in_array('ShowOther', $this->options->sidebarBlock)): ?>
-	<section class="widget">
-		<h3 class="widget-title"><?php _e('其它'); ?></h3>
+    <section class="widget">
+        <h3 class="widget-title"><?php _e('其它'); ?></h3>
         <ul class="widget-list">
             <?php if($this->user->hasLogin()): ?>
-				<li class="last"><a href="<?php $this->options->adminUrl(); ?>"><?php _e('进入后台'); ?> (<?php $this->user->screenName(); ?>)</a></li>
+                <li class="last"><a href="<?php $this->options->adminUrl(); ?>"><?php _e('进入后台'); ?> (<?php $this->user->screenName(); ?>)</a></li>
                 <li><a href="<?php $this->options->logoutUrl(); ?>"><?php _e('退出'); ?></a></li>
             <?php else: ?>
                 <li class="last"><a href="<?php $this->options->adminUrl('login.php'); ?>"><?php _e('登录'); ?></a></li>
@@ -62,6 +62,6 @@
             <li><a href="<?php $this->options->commentsFeedUrl(); ?>"><?php _e('评论 RSS'); ?></a></li>
             <li><a href="http://www.typecho.org">Typecho</a></li>
         </ul>
-	</section>
+    </section>
     <?php endif; ?>
 </div>
